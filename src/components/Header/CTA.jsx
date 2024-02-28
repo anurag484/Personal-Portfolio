1e05adf798976e6218dfdc0f2fa60d558e15da27
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2024-02-28 14:50:56 +0530

    added resume again

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
   const darkMode = theme.state.darkMode;
   return (
     <div className='ctn'>
-        <a style={{color:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href={"https://drive.google.com/drive/u/0/my-drive"} download="SameerKhanResume" className='btn'>Download CV</a>
+        <a style={{color:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href={"https://drive.google.com/file/d/1qtViipHx7Ec43Pbc2kz2g51YCP-FjO6O/view?usp=sharing"} download="SameerKhanResume" className='btn'>Download CV</a>
         <a style={{background:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
