18cf2c55098fe5529c4c0acd612c5f9904200a76
Anurag Ruwali <anuragruwali484@gmai.com>
2024-10-27 11:24:50 +0530

    changing resume link

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
   const darkMode = theme.state.darkMode;
   return (
     <div className='ctn'>
-        <a style={{color:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href={"https://drive.google.com/file/d/1fQ3Xh9GgGoCrtw9iu9SYi55rjDhD777K/view?usp=sharing "} download="AnuragRuwaliResume" className='btn' target='_blank' >Download CV</a>
+        <a style={{color:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href={"https://drive.google.com/file/d/1SB-Cm6LXPbgM1HiY1Bgd8LlG054k2hBh/view?usp=sharing "} download="AnuragRuwaliResume" className='btn' target='_blank' >Download CV</a>
         {/* <a style={{background:darkMode?"var(--color-primary-variant)":"",border:darkMode?"2px solid var(--color-primary-variant)":""}} href="#contact" className='btn btn-primary'>Let's Talk</a> */}
     </div>
   )
